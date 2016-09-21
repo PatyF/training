e7dd6214748bc0d96d6009a95171f652087929e1
Patricia Furtado <patyfurtado_1989@hotmail.com>
2016-09-20 21:46:00 -0300

    Retornando as categorias no curso

--- backend/app/models/course.rb
+++ backend/app/models/course.rb
@@ -3,4 +3,15 @@ class Course < ActiveRecord::Base
   belongs_to :instructor
   has_many :modulos
   has_and_belongs_to_many :categories
+
+  def builder_show
+    Jbuilder.new do |course|
+      course.id self.id
+      course.name self.name
+      course.keywords self.keywords
+      course.instructor_id self.instructor_id
+      course.available self.available
+      course.categories self.categories
+    end
+  end
 end
